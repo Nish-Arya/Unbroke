5e835892bd04bc32c64ea423fbc25b9ef065eaa0
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-07 12:18:58 -0400

    Set up auth back end routes

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,12 +6,15 @@ class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
+  username = db.Column(db.String(40), nullable = False)
   email = db.Column(db.String(255), nullable = False, unique = True)
+  hashed_password = db.Column(db.Binary(100), nullable=False)
+  monthly_income = db.Column(db.Integer, nullable=False)
 
   def to_dict(self):
     return {
       "id": self.id,
       "username": self.username,
-      "email": self.email
+      "email": self.email,
+      "monthly_income": self.monthly_income
     }
