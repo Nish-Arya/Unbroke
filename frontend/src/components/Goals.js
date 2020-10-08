c909ba5aec434347b3a553ad3b3beae506db1ba3
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-08 13:28:28 -0400

    Set up navbar and footer on home page

--- frontend/src/components/Goals.js
+++ frontend/src/components/Goals.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import "./Goals.css";
 
 export class Goals extends Component {
     render() {
         return (
-            <div>
-                Goals
-            </div>
-        )
+          <div className="goals">
+            <h1>Goals</h1>
+          </div>
+        );
     }
 }
 
