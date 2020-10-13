888831cf25ab78ef205fc634233012c08b10b3cf
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-13 12:28:51 -0400

    Set up goal react component

--- frontend/src/components/Goal.js
+++ frontend/src/components/Goal.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Goal.css';
 
 function Goal(props) {
     return (
-        <div>
-            {props.goal.description}
+      <div className="goal-container">
+        <div className="goal-buttons-holder">
+            <div>✔</div>
+            <div>X</div>
         </div>
-    )
+        <div className="goal-info-holder">
+            <div>Description: {props.goal.description}</div>
+            <div>Amount: {props.goal.amount}</div>
+            <div>Date: {props.goal.completion_month} {props.goal.completion_year}</div>
+            <div>Months Left: </div>
+        </div>
+        
+      </div>
+    );
 }
 
 export default Goal
