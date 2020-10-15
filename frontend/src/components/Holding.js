d055ca4ab7cd0a3baa6135790546762af714905a
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-15 16:15:02 -0400

    Set up Navlinks for holdings

---
+++ frontend/src/components/Holding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Holding() {
+    return (
+        <div>
+            Link
+        </div>
+    )
+}
+
+export default Holding;
--- frontend/src/components/Holdings.js
+++ frontend/src/components/Holdings.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react'
-import HoldingForm from './HoldingForm';
+import React, { useEffect } from "react";
+import {  NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setHoldings } from "../store/holdings";
+import HoldingForm from "./HoldingForm";
 import "./Holdings.css";
+import Holding from "./Holding";
 
-export class Holdings extends Component {
-    render() {
-        return (
-          <div className="holdings">
-            <div className="holdings-form-container">
-              <HoldingForm />
-            </div>
-            <div className="holdings-container">
-              <div className="holding-navlinks-container">
-                
-              </div>
-            </div>
+function Holdings() {
+
+    const user_id = useSelector((state) => state.auth.id);
+    const holdings = useSelector((state) => state.holdings);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      const loadHoldings = async () => {
+        const res = await fetch(`/api/holdings/${user_id}`);
+        res.data = await res.json();
+        if (res.ok) {
+          dispatch(setHoldings(res.data.holdings));
+        }
+      };
+      loadHoldings();
+    }, [dispatch, user_id]);
+    
+    return (
+      <div className="holdings">
+        <div className="holdings-form-container">
+          <HoldingForm />
+        </div>
+        <div className="holdings-container">
+          <div className="holding-navlinks-container">
+            {Object.values(holdings).map((holding) => {
+              return (
+                <NavLink
+                  key={holding.id}
+                  to={`/holding/${holding.id}`}
+                  className="holding-navlink"
+                  activeClassName="active-holding"
+                >
+                  <Holding holding={holding} key={holding.id} />
+                </NavLink>
+              );
+            })}
           </div>
-        );
-    }
+          <div className="holding-graph-container"></div>
+        </div>
+      </div>
+    );
 }
 
-export default Holdings
+export default Holdings;
