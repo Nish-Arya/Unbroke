d14a61a8f2b6736565379320c8526c4a4ca2856a
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-15 12:55:46 -0400

    Set up holding-form and add styling

---
+++ frontend/src/components/HoldingForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './HoldingForm.css'
+
+function HoldingForm() {
+
+    const [ticker, setTicker] = useState('');
+    const [buyPrice, setBuyPrice] = useState('');
+    const [numOfShares, setNumOfShares] = useState('');
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    const isFormValid = () => {
+      return ticker && buyPrice && numOfShares;
+    };
+
+    return (
+      <div className="container">
+        <form className="holding-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="ticker"
+            placeholder="Ticker"
+            value={ticker}
+            onChange={(e) => setTicker(e.target.value)}
+          />
+          <input
+            type="number"
+            name="buy_price"
+            placeholder="Buy Price"
+            value={buyPrice}
+            onChange={(e) => setBuyPrice(e.target.value)}
+          />
+          <input
+            type="number"
+            name="num_of_shares"
+            placeholder="Quantity"
+            value={numOfShares}
+            onChange={(e) => setNumOfShares(e.target.value)}
+          />
+          <button
+            className="holding-form-component"
+            type="submit"
+            disabled={!isFormValid()}
+          >
+            Create Holding
+          </button>
+        </form>
+      </div>
+    );
+};
+
+export default HoldingForm;
--- frontend/src/components/Holdings.js
+++ frontend/src/components/Holdings.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import HoldingForm from './HoldingForm';
 import "./Holdings.css";
 
 export class Holdings extends Component {
     render() {
         return (
           <div className="holdings">
-            <h1>Holdings</h1>
+            <div className="holdings-form-container">
+              <HoldingForm />
+            </div>
+            <div className="holdings-container"></div>
           </div>
         );
     }
