aac18d7e7ad5e2a56e930885096dc7302ef86289
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-27 13:28:26 -0400

    Integrate graph with data from api

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import "./Graph.css";
 
 function Graph(props) {
+
+    const [graphData, setGraphData] = useState([]);
+
+    useEffect(() => {
+      const loadGraph = async () => {
+        const res = await fetch(
+          `/api/holdings/current/${props.holding.ticker}`
+        );
+        res.data = await res.json();
+        if (res.ok) {
+          setGraphData(res.data.res.c);
+        }
+      };
+      loadGraph();
+    }, [props.holding.ticker]);
 
     const data = {
       labels: ["6wks", "5wks", "4wks", "3wks", "2wks", "1wk"],
       datasets: [
         {
           label: "Price History",
-          data: [33, 53, 85, 41, 44, 650],
+          data: graphData,
           fill: false,
           backgroundColor: "rgba(75,192,192,0.2)",
           borderColor: "rgba(75,192,192,1)",
