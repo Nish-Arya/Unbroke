19ceb103c453c15faaf56af8f68d88d04eca223b
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-27 12:54:50 -0400

    Set up line chart in graph component

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Line } from "react-chartjs-2";
 
 function Graph(props) {
+
+    const data = {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      datasets: [
+        {
+          label: "First dataset",
+          data: [33, 53, 85, 41, 44, 65],
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        },
+        {
+          label: "Second dataset",
+          data: [33, 25, 35, 51, 54, 76],
+          fill: false,
+          borderColor: "#742774",
+        },
+      ],
+    };
+    
     return (
-        <div>
-            {props.holding.ticker}
+      <div>
+        {props.holding.ticker}
+        <div className="graph-holder">
+          <Line data={data} />
         </div>
-    )
+      </div>
+    );
 }
 
 export default Graph;
